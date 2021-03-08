2eaf0b36d235ee5c85d333436630b339402be240
Simranpreetkaur <harsim610@gmail.com>
2021-03-08 00:37:37 -0500

    Reverse order of array

--- src/main/java/com/lambton/classWork.java
+++ src/main/java/com/lambton/classWork.java
@@ -4,11 +4,15 @@ public class classWork
 {
     public static void main(String[] args)
     {
-        int[] myArray = {5,89,65,14,40,30,60};
-        System.out.println("\n Array :");
+        int[] myArray = {5, 89, 65, 14, 40, 30, 60, 27, 10, 50};
+        System.out.println("Array :");
         for (int x : myArray)
         {
             System.out.print(x + "  ");
+        }
+        System.out.println("\n\nArray in reverse order:");
+        for (int i = myArray.length - 1 ; i >= 0 ; i-- )  {
+            System.out.print(myArray[i] + "  ");
         }
     }
 }
