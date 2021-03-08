2e23b0676a8f44079a327ffab85def0a975df0f0
Simranpreetkaur <harsim610@gmail.com>
2021-03-08 00:31:06 -0500

    initailizing of array

--- src/main/java/com/lambton/classWork.java
+++ src/main/java/com/lambton/classWork.java
@@ -1,5 +1,14 @@
 package com.lambton;
 
-public class classWork {
-
+public class classWork
+{
+    public static void main(String[] args)
+    {
+        int[] myArray = {5,89,65,14,40,30,60};
+        System.out.println("\n Array :");
+        for (int x : myArray)
+        {
+            System.out.print(x + "  ");
+        }
+    }
 }
